13ce9ee7539c5eccdd851eed9948563f33dbe1b0
Dave Boku <dsboku26@colby.edu>
2025-01-27 13:00:19 +0100

    chore: clean up var setup.

--- services/supabaseClient.js
+++ services/supabaseClient.js
@@ -2,7 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 
 // Replace with your Supabase credentials
 const SUPABASE_URL = "https://veecqofclsibnudlhahx.supabase.co";
-const SUPABASE_SERVICE = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZlZWNxb2ZjbHNpYm51ZGxoYWh4Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczMjczNTEyMCwiZXhwIjoyMDQ4MzExMTIwfQ.dWLb1HG1fyAckpMRRx43COF57EAb8Q5FjBcCeouQ-zc";
 
 
 // export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
